afe2e224a2dcb2fb8b885e55b5ee3fc390568389
James Goodall <jatgoodall@protonmail.com>
2020-11-14 13:18:07 +0000

    started work on basic networking

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
-func main(){
-	print("hello")
+import (
+	"io"
+	"net/http"
+	"net"
+)
+
+func hello(w http.ResponseWriter, r *http.Request){
+	io.WriteString(w, "Hello, World")
 }
+
+
+func connectToGame() net.Conn {
+	conn, err := net.Dial("tcp", "127.0.0.1:46920")
+	if err != nil {
+		panic("Failed to connect to Game Server")
+	}
+	return conn
+}
+
+
+
+func main(){
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+
+
+
+
+
