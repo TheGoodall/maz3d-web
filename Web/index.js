ccc6dcc53214913d5b18973e1ff915ff92533f27
Christopher <Cskywalker1@gmail.com>
2020-11-14 18:18:38 +0000

    Map will now be rendered. However, there is a minor bug (see TODO: line 69 (nice!)).

--- Web/index.js
+++ Web/index.js
@@ -16,7 +16,7 @@ var placeholder_map_matrix2 =   [
     [0, 1]
 ]
 
-var map_matrix = placeholder_map_matrix2;
+var map_matrix = placeholder_map_matrix;
 var map_width = placeholder_map_width;
 var map_height = placeholder_map_height;
 var canvas_scale = 100;
@@ -35,9 +35,13 @@ function setup() {
 function draw() {
     background(0, 0, 0)
 
-    // TODO: Remove two below when done.
-    // ellipse(50, 50, 80, 80)
-    square(30, 20, 55)
+
+    for (var i = 0; i < empty_location.length; i++) {
+        // console.log(empty_location)
+        // console.log(i, empty_location[i])
+        square(empty_location[i][0], empty_location[i][1], canvas_scale)
+    }
+    // square(30, 20, 55)
 }
 
 
@@ -53,6 +57,7 @@ function create_map() {
     map_width = canvas_scale * map_y
     map_height = canvas_scale * map_x
 
+    // TODO: Remove logs below when done.
     console.log("PRICK")
 
     console.log(map_width)
@@ -61,22 +66,23 @@ function create_map() {
     console.log("CUNT")
 
     // Iterate through map matrix and draw map.
+    // TODO: Fix bug here: Keeps rendering an extra line at the bottom.
     for (var x = 0; x < map_x; x++) {
-        for (var y = 0; y < map_y; y ++) {
+        for (var y = 0; y < map_y; y++) {
 
             // TODO: Delete these console.log
             console.log(y, x)
             console.log(map_matrix[y][x])
 
             // Get location of where square should be rendered.
-            render_location_x = ((canvas_scale/2) + canvas_scale * x)
-            render_location_y = ((canvas_scale/2) + canvas_scale * y)
+            render_location_x = (canvas_scale * x)
+            render_location_y = (canvas_scale * y)
 
             switch (map_matrix[y][x]) {
 
                 // If empty.
                 case 0:
-                    square(render_location_x, render_location_y, canvas_scale)
+                    empty_location.push([render_location_x, render_location_y])
                     break;
 
                 // If wall.
