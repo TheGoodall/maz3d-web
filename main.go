84d4ebd22ef2b33cd15ed8f6f8c9630a582aa9e2
James Goodall <jatgoodall@protonmail.com>
2020-11-14 17:39:52 +0000

    added TCP Server

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"net/http"
+	"time"
 	"net"
 	"github.com/alexandrevicenzi/go-sse"
 )
 
 
 func handleConnection(conn net.Conn) {
-	defer conn.Close()
+
 
 }
 
@@ -17,13 +18,15 @@ func startTCPServer(){
 	if err != nil {
 		panic("Error starting server")
 	}
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			panic("Could not accept connection")
-		}
-		handleConnection(conn)
+	conn, err := ln.Accept()
+	if err != nil {
+		panic("Could not accept connection")
 	}
+	defer conn.Close()
+
+
+
+
 }
 
 func startHTTPServer(gamestartC chan string, playerlocC chan string, playercountC chan chan int){
@@ -31,20 +34,22 @@ func startHTTPServer(gamestartC chan string, playerlocC chan string, playercount
 	defer s.Shutdown()
 
 	http.Handle("/events/", s)
+	static := http.FileServer(http.Dir("./Web"))
+	box := packr.NewBox("./Web")
+	http.Handle("/", static)
 
 
 	go http.ListenAndServe(":8080", nil)
 
-	clients := make(map[int]sse.Client)
 	for {
 		select {
 		case mapjson := <-gamestartC:
-			
 			s.SendMessage("/events/game", sse.SimpleMessage(mapjson))
 		case playerlocation:=  <-playerlocC:
-
+			s.SendMessage("/events/game", sse.SimpleMessage(playerlocation))
 		case pcC := <-playercountC:
-			pcC <- s.GetChannel("/events/game").ClientCount()
+			k, _ :=s.GetChannel("/events/game")
+			pcC <- k.ClientCount()
 		}
 	}
 
@@ -56,7 +61,10 @@ func main(){
 	playercountC := make(chan chan int)
 
 	go startHTTPServer(gamestartC, playerlocC, playercountC)
+	go startTCPServer()
 
+
+	time.Sleep(1*time.Second)
 
 }
 
